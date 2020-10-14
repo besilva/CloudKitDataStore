228b73d5a1b770099eae4781b3f461f395b9cf6d
Bernardo Silva <silva98.bernardo@gmail.com>
2020-10-14 11:28:52 -0300

    minimum ios Version

--- Package.swift
+++ Package.swift
@@ -1,10 +1,13 @@
-// swift-tools-version:5.3
+// swift-tools-version:5.2
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
 
 let package = Package(
     name: "CloudKitDataStore",
+    platforms: [
+           .macOS(.v10_10), .iOS(.v11), .tvOS(.v9), .watchOS(.v2)
+    ],
     products: [
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .library(
